5d90c0a0c5be6a399165ec7593d9e830958beca6
Eunsoo Roh <roh7@illinois.edu>
2014-03-15 16:29:40 -0500

    Pulled option definitions out of main() function.

--- src/server.py
+++ src/server.py
@@ -14,16 +14,17 @@ import tornado.ioloop
 import tornado.web
 from Application import Application
 
+# Global Option Configuration #
+tornado.options.define( "listen_port", default=9999,
+                        help="Server Listen Port", type=int )
+tornado.options.define( "listen_address", default='',
+                        help="Server Listen Address", type=str )
+
 ### Primary Entry Point ###
 
 ##  The primary entry point function for the CS411 project backend, which 
 #   instantiates the backend to listen for API requests.
 def main():
-    # Global Option Configuration #
-    tornado.options.define( "listen_port", default=9999,
-                            help="Server Listen Port", type=int )
-    tornado.options.define( "listen_address", default='',
-                            help="Server Listen Address", type=str )
 
     # Start-Up Logic #
     tornado.options.parse_command_line()
