3b516eed12a74606af2f96732fe1bf62f80a09bb
tom <tom@tom-Lenovo-IdeaPad-P500.(none)>
2014-04-28 01:26:07 -0500

    added aggregation.py and scheduler.py

---
+++ datbigcuke/aggregation.py
@@ -0,0 +1,106 @@
+#functions to decide if a deadline should be aggregated
+#(by Tom)
+
+from datetime import timedelta, datetime
+import numpy as np
+
+#transforms a list of datetimes to a list of integers
+#representing those times and returns the new list
+#the return list will be a numpy array
+#(this representaion is the number of seconds
+# since jan 1, 2014)
+def transform_to_ints(deadlines):
+    begining_of_time = datetime(2014, 1, 1)
+    values = []
+    for d in deadlines:
+        delta = d - begining_of_time
+        values.append(delta.seconds + 3600*24 * delta.days)
+    return np.array(values)
+    
+
+#transforms a integer representation as from above
+#back into a datetime and returns it
+def transform_to_datetime(rep):
+    begining_of_time = datetime(2014, 1, 1)
+    delta = timedelta(np.floor(rep / (3600*24)), int(rep % (3600*24)))
+    return begining_of_time + delta
+    
+#given a list of datetimes,
+#returns an array of transformed datetimes
+#which contains only the deadlines close to a
+#discovered central value
+def find_central_deadlines(deadlines):
+    #calculate besic statics on the deadlines
+    transformed_deadlines = np.array(transform_to_ints(deadlines))
+    average = np.average(transformed_deadlines)
+    deviation = np.std(transformed_deadlines)
+    
+    #print sorted(transformed_deadlines.tolist())
+    
+    #get a list of just those within one standard deviation
+    close_deadlines = []
+    for d in transformed_deadlines:
+        if d > average - deviation and d < average + deviation:
+            close_deadlines.append(d)
+    close_deadlines = np.array(close_deadlines)
+    
+    #print sorted(close_deadlines.tolist())
+    
+    #repeat the process again, getting very close deadlines
+    close_average = np.average(close_deadlines)
+    close_deviation = np.std(close_deadlines)
+    very_close_deadlines = []
+    for d in close_deadlines:
+        if d > close_average - close_deviation and d < close_average + close_deviation:
+            very_close_deadlines.append(d)
+    very_close_deadlines = np.array(very_close_deadlines)
+    
+    #print sorted(very_close_deadlines.tolist())
+    
+    return very_close_deadlines
+    
+#Decides if a deadline should be aggregated
+#deadlines is a list of datetimes representing
+#all the candidates for the deadline
+#threshold is the number of deadlines that must
+#lie in the center to cause aggregation
+def should_aggregate(deadlines, threshold):
+    center = find_central_deadlines(deadlines)
+    return len(center) > threshold
+
+#Aggregates a list of datetime deadlines
+#returns the datetime deadline that is most
+#likely to be the real dealine from this list
+def aggregate(deadlines):
+    center = find_central_deadlines(deadlines)
+    deadline = max(set(center), key=center.tolist().count)
+    return transform_to_datetime(deadline)
+    
+    
+#TESTING
+deadlines1 = [datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 45, 0, 0),
+              datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 29, 0, 0),
+              datetime(2014, 4, 26, 16, 29, 0, 0),
+              datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 17, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 29, 0, 0),
+              datetime(2014, 4, 26, 16, 25, 0, 0),
+              datetime(2014, 4, 26, 16, 25, 0, 0),
+              datetime(2014, 4, 26, 16, 29, 0, 0),
+              datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 25, 0, 0),
+              datetime(2014, 4, 26, 16, 28, 0, 0),
+              datetime(2014, 4, 26, 16, 28, 0, 0),
+              datetime(2014, 4, 26, 16, 28, 0, 0)]
+             
+deadlines2 = [datetime(2014, 4, 26, 16, 30, 0, 0),
+              datetime(2014, 4, 26, 16, 29, 0, 0),
+              datetime(2014, 4, 26, 16, 28, 0, 0),
+              datetime(2014, 4, 26, 16, 27, 0, 0),
+              datetime(2014, 4, 26, 16, 31, 0, 0),
+              datetime(2014, 4, 26, 16, 32, 0, 0),
+              datetime(2014, 4, 26, 16, 33, 0, 0)]
---
+++ datbigcuke/scheduler.py
@@ -0,0 +1,137 @@
+#Functions for scheduling a meeting time for a group
+#(by Tom)
+
+from datetime import timedelta, datetime, date, time
+import math
+
+#returns a datetime or time representing the
+#start of the m minute interval
+#in which the given datetime or time t lies
+def time_interval_start(t, m):
+    minutes = int(m * math.floor(float(t.minute)/m))
+    if(type(t) is datetime):
+        return datetime(t.year, t.month, t.day, t.hour, minutes)
+    if(type(t) is time):
+        return time(t.hour, minutes)
+    
+#computes the possible meeting times
+#group_members is a dictionary of members whose
+#  keys are identifiers of the member
+#  values are lists of 2-tuples,
+#    containing a start time (datetime) and end time (datetime)
+#  these tell when that group member is unavailable
+#deadline is a datetime specifying before when the meeting must occur
+#duration is a timedelta specifying how long the meeting should be
+#off_limits_start and off_limits_end are times
+#  which tell when the meeting should not be scheduled each day
+#This function returns a list of 2-tuples describing possible meeting times
+#  first value being the time of the meeting
+#  second value being a list of group members
+#    who are busy at some pointduring the meeting, if any
+#  and this list is sorted first by time, then by availability
+#
+#Important assumptions:
+#the off_limits time should cover a day change
+#  if not, return an error list
+#the earliest schedulable time is an hour from now
+def schedule_meeting(group_members, deadline, duration, off_limits_start, off_limits_end):
+    
+    #check off_limits assumption    
+    if(off_limits_start <= off_limits_end):
+        return [(deadline, ['error: off limits times are bad'])]
+        
+    #m is the interval length, this must be 0 < m < 60
+    m = 15
+    
+    #convert the given times to the intervals they are in
+    overall_start = time_interval_start(datetime.now() + timedelta(hours=1, minutes=m), m)
+    overall_end = time_interval_start(deadline, m)
+    day_start = time_interval_start(off_limits_end, m)
+    day_end = time_interval_start(off_limits_start, m)
+    
+    #create a structure that maps usable time intervals
+    #to lists of members busy during that interval
+    #and a similar structure that only has fully usable slots of length duration
+    #this initializes all usable times to an empty list
+    intervals = {}
+    options = {}
+    
+    day = overall_start.date()
+    while(day <= overall_end.date()):
+        t = day_start
+        end = day_end
+        #start later on the first day, or end earlier on the last day, if necessary
+        if(day == overall_start.date() and t < overall_start.time()):
+            t = overall_start.time()
+        if(day == overall_end.date() and overall_end.time() < end):
+            end = overall_end.time()
+            
+        while(t < end):
+            intervals[datetime.combine(day, t)] = []
+            if((datetime.combine(day, t) + duration).time() <= end):
+                options[datetime.combine(day, t)] = []
+            t = (datetime.combine(day, t) + timedelta(minutes=m)).time()
+            
+        day = day + timedelta(days=1)
+        
+    #put busy group members in the appropriate intervals
+    for member in group_members:
+        for appointment in group_members[member]:
+            t = time_interval_start(appointment[0], m)
+            end = time_interval_start(appointment[1], m)
+            while(t < end):
+                if(t in intervals):
+                    intervals[t].append(member)
+                t = time_interval_start(t + timedelta(minutes=m), m)
+                
+    #use the intervals to fill the options with busy group members
+    for option in options:
+        t = option
+        end = time_interval_start(option + duration, m)
+        while(t < end):
+            options[option] += intervals[t]
+            t = t + timedelta(minutes=m)
+        
+    #zip the options into a sortable form
+    option_tuples = []
+    for option in options:
+        option_tuples.append((len(set(options[option])), len(options[option]), option))
+        
+    #construct the result
+    result = []
+    for option in sorted(option_tuples):
+        result.append((option[2], list(set(options[option[2]]))))
+    
+    return result
+    
+    
+    
+    
+#TESTING (note these won't work after today (April 17))
+t1 = datetime.now()
+t2 = (t1 + timedelta(minutes=7)).time()
+
+print t1
+print time_interval_start(t1, 5)
+print t2
+print time_interval_start(t2, 5)
+print ""
+
+group_members = {}
+bob = [(datetime(2014, 4, 17, 12), datetime(2014, 4, 17, 18)),
+       (datetime(2014, 4, 18, 12), datetime(2014, 4, 18, 18))]
+sue = [(datetime(2014, 4, 17, 19), datetime(2014, 4, 17, 23)),
+       (datetime(2014, 4, 18, 20), datetime(2014, 4, 18, 23))]
+joe = [(datetime(2014, 4, 17, 14), datetime(2014, 4, 17, 17)),
+       (datetime(2014, 4, 18, 14), datetime(2014, 4, 18, 18, 30))]
+group_members['bob'] = bob
+group_members['sue'] = sue
+group_members['joe'] = joe
+deadline = datetime(2014, 4, 18, 22)
+duration = timedelta(hours=1)
+off_limits_start = time(21)
+off_limits_end = time(12)
+
+schedule = schedule_meeting(group_members, deadline, duration, off_limits_start, off_limits_end)
+for s in schedule:
+    print s[0], s[1]
