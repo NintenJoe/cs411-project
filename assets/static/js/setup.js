5ef918c159fc3d1f17997f0630931f9e0d24afc0
Joseph Ciurej <ciurej2@illinois.edu>
2014-04-28 23:55:41 -0500

    Finished up the initial implementation of the autocomplete fields.

--- assets/static/js/setup.js
+++ assets/static/js/setup.js
@@ -50,48 +50,39 @@ function main()
 
 	// Set up Autocomplete Fields ///
 	{
-		var substringMatcher = function(strs) {
-			return function findMatches(q, cb) {
-			var matches, substringRegex;
-			 
-			// an array that will be populated with substring matches
-			matches = [];
-			 
-			// regex used to determine if a string contains the substring `q`
-			substrRegex = new RegExp(q, 'i');
-			 
-			// iterate through the pool of strings and for any string that
-			// contains the substring `q`, add it to the `matches` array
-			$.each(strs, function(i, str) {
-			if (substrRegex.test(str)) {
-			// the typeahead jQuery plugin expects suggestions to a
-			// JavaScript object, refer to typeahead docs for more info
-			matches.push({ value: str });
-			}
-			});
-			 
-			cb(matches);
-			};
-			};
-			 
-			var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-			'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-			'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-			'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-			'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-			'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-			'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-			'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-			'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-			];
+		var typeahead_options = { hint: true, highlight: true, minlength: 2 };
 
-		$( "#new_user_email" ).typeahead( { hint: true, highlight: true, minlength: 2 },
-			{ name: "states", displaykey: "value", source: substringMatcher(states) } );
+		var emailFinder = new Bloodhound( {
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace( "value" ),
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			remote: "../get-emails"
+		} );
+		emailFinder.initialize();
+		$( "#new_user_email" ).typeahead( typeahead_options,
+			{ name: "emails", displaykey: "value", source: emailFinder.ttAdapter() } );
+
 		// TODO: Only allow this feature if the user can add courses.
-		$( "#group_name" ).typeahead( { hint: true, highlight: true, minlength: 2 },
-			{ name: "states", displaykey: "value", source: substringMatcher(states) } );
-		$( "#deadline_name" ).typeahead( { hint: true, highlight: true, minlength: 2 },
-			{ name: "states", displaykey: "value", source: substringMatcher(states) } );
+		var courseFinder = new Bloodhound( {
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace( "value" ),
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			remote: "../get-courses"
+		} );
+		courseFinder.initialize();
+		$( "#group_name" ).typeahead( typeahead_options,
+			{ name: "emails", displaykey: "value", source: courseFinder.ttAdapter() } );
+
+		var deadlineFinder = new Bloodhound( {
+			datumTokenizer: Bloodhound.tokenizers.obj.whitespace( "value" ),
+			queryTokenizer: Bloodhound.tokenizers.whitespace,
+			remote: "../get-deadlines"
+		} );
+		deadlineFinder.initialize();
+		$( "#deadline_name" ).typeahead( typeahead_options,
+			{ name: "emails", displaykey: "value", source: deadlineFinder.ttAdapter() } );
+
+		// TODO: Remove this example code.
+		/*$( "#group_name" ).typeahead( { hint: true, highlight: true, minlength: 2 },
+			{ name: "states", displaykey: "value", source: substringMatcher(states) } );*/
 	}
 
 	// Bind AJAX Requests to Fields //
